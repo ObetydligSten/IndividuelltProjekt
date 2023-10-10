f9f9812235805acf7229c46d8b4a5d6cf5720401
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-10-10 11:03:13 +0200

    gjorde kontonamn till icke case sensitive + ändrat formatet på saldon till att visa endast två decimaler

--- IndividuelltProjekt/Program.cs
+++ IndividuelltProjekt/Program.cs
@@ -7,20 +7,20 @@
         static int LoggedInCode = 0;
 
         //Dessa ligger som globala variablar så inte summan i kontona ska reseta när man kollar sitt konto.
-        static string[] rasmusAName = new string[3] { "Sparkonto", "Lönekonto", "Resekonto" };
+        static string[] rasmusAName = new string[3] { "SPARKONTO", "LÖNEKONTO", "RESEKONTO" };
         static double[] rasmusASum = new double[3] { 67324.71, 9023.40, 36041.00 };
 
-        static string[] benAName = new string[1] { "Sparkonto", };
+        static string[] benAName = new string[1] { "SPARKONTO", };
         static double[] benASum = new double[1] { 333333.00 };
 
-        static string[] stenAName = new string[4] { "Sparkonto", "Lönekonto", "Resekonto", "Huskonto" };
+        static string[] stenAName = new string[4] { "SPARKONTO", "LÖNEKONTO", "RESEKONTO", "HUSKONTO" };
         static double[] stenASum = new double[4] { 60324.09, 9023.40, 36041.00, 3500000 };
 
-        static string[] kurtAName = new string[2] { "Sparkonto", "Lönekonto" };
+        static string[] kurtAName = new string[2] { "SPARKONTO", "LÖNEKONTO" };
         static double[] kurtASum = new double[2] { 999666.16, 31432.40 };
 
-        static string[] svenAName = new string[3] { "Sparkonto", "Lönekonto", "Resekonto" };
-        static double[] svenASum = new double[3] { 156314.93, 6400.02, 3.00 };
+        static string[] svenAName = new string[5] { "SPARKONTO", "LÖNEKONTO", "RESEKONTO", "HUSKONTO", "MATKONTO" };
+        static double[] svenASum = new double[5] { 156314.93, 6400.02, 3.00, 11111.75, 3377.90 };
 
         static void Main(string[] args)
         {
@@ -99,35 +99,43 @@
                 Console.WriteLine("[2] Överföring mellan konton");
                 Console.WriteLine("[3] Ta ut pengar");
                 Console.WriteLine("[4] Logga ut");
-                int menuChoice = Convert.ToInt32(Console.ReadLine());
-
-                //Varje case skickar till accounts metoden vem som är inloggad och vilket menyval som gjordes.
-                switch (menuChoice)
+                if (Int32.TryParse(Console.ReadLine(), out int menuChoice))
                 {
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("Inloggad som " + LoggedInUser);
-                        Accounts(LoggedInUser, menuChoice);
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Console.WriteLine("Inloggad som " + LoggedInUser);
-                        Accounts(LoggedInUser, menuChoice);
-                        break;
-                    case 3:
-                        Console.Clear();
-                        Console.WriteLine("Inloggad som " + LoggedInUser);
-                        Accounts(LoggedInUser, menuChoice);
-                    break;
-                    case 4:
-                        Console.WriteLine("Du loggas ut. . .");
-                        Console.ReadKey();
-                        Login();
-                        break;
-                    default:
-                        Console.WriteLine("Ogiltigt val, skriv in en siffra [1-4]. . .");
-                        break;
+                    //Varje case skickar till accounts metoden vem som är inloggad och vilket menyval som gjordes.
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Inloggad som " + LoggedInUser);
+                            Accounts(LoggedInUser, menuChoice);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Inloggad som " + LoggedInUser);
+                            Accounts(LoggedInUser, menuChoice);
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Inloggad som " + LoggedInUser);
+                            Accounts(LoggedInUser, menuChoice);
+                            break;
+                        case 4:
+                            Console.WriteLine("Du loggas ut. . .");
+                            Console.ReadKey();
+                            Login();
+                            break;
+                        default:
+                            Console.WriteLine("Ogiltigt val, skriv in en siffra [1-4]. . .");
+                            Console.ReadKey();
+                            break;
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("Skriv in en siffra. . .");
+                    Console.ReadKey();
+                }
+                   
             }
         }
 
@@ -146,7 +154,7 @@
                     for (int i = 0; i < rasmusAName.Length; i++)
                     {
                         Console.Write(rasmusAName[i] + ": ");
-                        Console.WriteLine(rasmusASum[i] + " SEK");
+                        Console.WriteLine(rasmusASum[i].ToString("0.00") + " SEK");
                     }
                     Console.WriteLine("\nTryck ENTER för att gå tillbaka till menyn. . .");
                     Console.ReadKey();
@@ -158,11 +166,11 @@
                     for (int i = 0; i < rasmusAName.Length; i++)
                     {
                         Console.Write(rasmusAName[i] + ": ");
-                        Console.WriteLine(rasmusASum[i] + " SEK");
+                        Console.WriteLine(rasmusASum[i].ToString("0.00") + " SEK");
                     }
                     Console.WriteLine("\nVilka konton vill du överföra pengar mellan?");
-                    string accountFrom = Console.ReadLine();
-                    string accountTo = Console.ReadLine();
+                    string accountFrom = Console.ReadLine().ToUpper();
+                    string accountTo = Console.ReadLine().ToUpper();
                     for (int i = 0; i < rasmusAName.Length; i++)
                     {
                         if (accountFrom == rasmusAName[i])
@@ -177,8 +185,9 @@
                                     {
                                         rasmusASum[i] -= transfer;
                                         rasmusASum[j] += transfer;
-                                        Console.WriteLine($"Överföring klar, nytt saldo är: \n\n{rasmusAName[i]}: {rasmusASum[i]} SEK" +
-                                            $"\n{rasmusAName[j]}: {rasmusASum[j]} SEK\n\nTryck ENTER för att gå tillbaka till menyn ");
+                                        Console.WriteLine($"Överföring klar, nytt saldo är: \n\n{rasmusAName[i]}: {rasmusASum[i].ToString("0.00")}" +
+                                            $" SEK" +$"\n{rasmusAName[j]}: {rasmusASum[j].ToString("0.00")}" +
+                                            $" SEK\n\nTryck ENTER för att gå tillbaka till menyn ");
                                         Console.ReadKey();                                       
                                         Menu();
                                     }
@@ -225,10 +234,10 @@
                     for (int i = 0; i < rasmusAName.Length; i++)
                     {                        
                         Console.Write(rasmusAName[i] + ": ");
-                        Console.WriteLine(rasmusASum[i] + " SEK");
+                        Console.WriteLine(rasmusASum[i].ToString("0.00") + " SEK");
                     }
                     Console.WriteLine("\nVilket Konto vill du ta ut pengar från?");
-                    string accountFrom = Console.ReadLine();
+                    string accountFrom = Console.ReadLine().ToUpper();
                     for (int i = 0;i < rasmusAName.Length;i++)
                     {
                         if (accountFrom == rasmusAName[i])
@@ -241,7 +250,7 @@
                                 if (transfer > 0 && (rasmusASum[i] - transfer) >= 0 && codeCheck == LoggedInCode)
                                 {
                                     rasmusASum[i] -= transfer;
-                                    Console.WriteLine($"Uttag klart, nya saldot är \n\n{rasmusAName[i]}: {rasmusASum[i]} SEK" +
+                                    Console.WriteLine($"Uttag klart, nya saldot är \n\n{rasmusAName[i]}: {rasmusASum[i].ToString("0.00")} SEK" +
                                         $"\n\nTryck ENTER för att gå tillbaka till menyn");
                                     Console.ReadKey();
                                     Menu();
